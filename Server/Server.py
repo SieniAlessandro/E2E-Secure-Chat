8579c0457282f7ec79c3b58f423e49994e8bab9d
Alessandro Sieni <alexander.sieni@gmail.com>
2018-09-25 16:42:59 +0200

    Aggiunta scheletro Server
    Aggiunto lo scheletro del server

---
+++ Server/Server.py
@@ -0,0 +1,24 @@
+import socket
+from threading import Thread
+from SocketServer import ThreadingMixIn
+
+class Server:
+    def __init__(self,port):
+        self.ip = "0.0.0.0"
+        self.port = port
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+        self.server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1);
+        self.server.bind((self.ip,self.port))
+        self.clients = []
+        print "Server iniziallizato"
+
+    def listen(self):
+        while True:
+            self.server.listen(50)
+            print "In attesa di richieste..."
+            (conn, (ip,port)) = self.server.accept()
+            print "Nuova richiesta arrivata.."
+            newClient = ClientHandler(conn,ip,port);
+            newClient.start();
+            self.clients.append(newClient);
+        
