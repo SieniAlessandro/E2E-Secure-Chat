6fc75832d4308b5ee3da98d73bf8c5c679166bd6
maghero <34124449+maghero@users.noreply.github.com>
2018-10-07 17:53:06 +0200

    Bozza per la Classe Client, @Amedeo @Alessandro controllate se possono andar bene in generale; @Amedeo dobbiamo metterci d'accordo sul tipo di variabile che usi per i messaggi e poi dobbiamo vedere come gestisce la funzione send la GUI fatta con tkinter [mette implicitamente degli Event?] nota minore: nel server ho solo cambiato le print, non so se con python 3 è obbligatorio mettere le parentesi anche per la funzione print, ma senza non esegue

--- Client/client.py
+++ Client/client.py
@@ -1,19 +1,65 @@
-import socket
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
 
-host = socket.gethostname()
-port = 1745
-BUFFER_SIZE = 2000
-MESSAGE = raw_input("tcpClientA: Enter message/ Enter exit:")
+class Client:
 
-tcpClientA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpClientA.connect((host, port))
+    def __init__(self, hostServer, portServer, bufferSize):
+        self.hostServer = hostServer; #IPv4 Address of the server
+        self.portServer = 1745
+        self.BUFFER_SIZE = 2000
 
-while MESSAGE != 'exit':
-    tcpClientA.send(MESSAGE)
-    data = tcpClientA.recv(BUFFER_SIZE)
-    print " Client2 received data:", data
-    MESSAGE = raw_input("tcpClientA: Enter message to continue/ Enter exit:")
+    #Functions to communicate with Server#
 
-tcpClientA.close()
-print("Ora funziona")
-print("Bravoh!")
+    def connectServer(self):
+        self.connectionToServer = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcpClientA.connect((host, port))
+
+    def sendServer(self, ...):
+
+    ######################################
+
+    #Functions for the FrontEnd#
+
+    def register(self, ...):
+
+    def login(self, ...):
+
+    def sendMessage(self, ...):
+
+    ############################
+
+    #Functions to communicate with others Clients
+
+    #UDP connection with clients?
+    def connectClient(self, ...):
+
+    def receiveMessage(self, ...):
+        while True:
+            try:
+                msg = sockerRicezione.recv(self.BUFFER_SIZE).decode("utf8")
+                #insert into a list of the front end (tkinter.Listbox) @Amedeo
+                ...
+            except OSError: #The other client could have left the chat
+                #the Thread that listens can put to do something else or closed
+                break
+
+    def send(self, event=None, ...):  # event is passed by binders of the tkinter GUI automatically
+        #Handles sending of messages
+        msg = my_msg.get()
+        my_msg.set("")  # Clears input field.
+        if(self.clientReceiverOnline(index))
+            client_socket.send(bytes(msg, "utf8"))
+        else
+            server_socket.send(bytes(msg, "utf8"))
+        if msg == "{quit}":
+            client_socket.close()
+            top.quit()
+
+    def onClosing(event=None): #clean up before close
+        #close the socket connection
+        my_msg.set("{quit}")
+        send()
+
+    #Functions for the Security#
+    ##TO DO TO DO  TO DO TO DO##
+    ############################
