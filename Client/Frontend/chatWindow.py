f5cd554a6c9a0e55470c877a519089835ac57ffd
APochiero <32522627+APochiero@users.noreply.github.com>
2018-10-08 08:48:49 +0200

    BoxMessage, InputBar, chatWindow added

---
+++ Client/Frontend/chatWindow.py
@@ -0,0 +1,7 @@
+from tkinter import *
+
+class chatWindow(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master, bd=3, highlightthickness=1)
+
+        # self.charBar = Frame(self, background)
