3822dbbfab21fdea4a5ad23790606f5722b25676
Alessandro Sieni <Alexander.sieni@gmail.com>
2018-10-09 10:04:19 +0200

    Aggiunta gestione dei messaggi per il login con relativo controllo di presenza online

--- Server/ClientHandler.py
+++ Server/ClientHandler.py
@@ -38,12 +38,19 @@ class ClientHandler(Thread):
                 print("Login")
                 print("Ip del client: " + self.ip)
                 param = msgs[1].split(',')
-                if(self.DB.userIsPresent(*param) == 1):
-                    self.conn.sendall("Login OK".encode('utf-16'))
+                response = ""
+                #Check if this user is already Logged In
+                if self.ip in self.OnlineClients.values:
+                    response = "?|"+str(-1)
+                elif self.DB.userIsPresent(*param) == 1:
+                    response = "?|"+str(1)
+                    #self.conn.sendall("Login OK".encode('utf-16'))
                     #self.OnlineClients.append(User(param[0]))
                     self.OnlineClients[param[0]] = self.ip
                 else:
-                    self.conn.sendall("Username o password errati".encode('utf-16'))
+                    #self.conn.sendall("Username o password errati".encode('utf-16'))
+                    response = "?|"+str(0)
+                self.conn.send(response.encode('utf-16'))
                 print(self.OnlineClients)
             elif msgs[0] == '3':
                 print("Richiesta di utente online")
