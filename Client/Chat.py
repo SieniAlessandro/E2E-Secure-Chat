20d61c7c2ac744742057cfa309836df3784dcaf3
maghero <34124449+maghero@users.noreply.github.com>
2018-10-17 12:10:58 +0200

    res

--- Client/Chat.py
+++ Client/Chat.py
@@ -1,24 +1,26 @@
 from tkinter import *
 from ChatList import ChatList
 from ChatWindow import *
+from client import Client
+import os
 
 class ChatGUI(Tk):
     backgroundWindow = '#47476b'
     backgroundItems = '#29293d'
     def __init__(self):
         Tk.__init__(self)
-        w = 1024 # width for the Tk root
-        h = 720 # height for the Tk root
 
         ws = self.winfo_screenwidth() # width of the screen
         hs = self.winfo_screenheight() # height of the screen
+
+        w = ws*1.5/3 # width for the Tk root
+        h = hs*2.5/4 # height for the Tk root
 
         x = (ws/2) - (w/2)
         y = (hs/2) - (h/2)
 
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        self.title("Chat")
-        self.geometry('1024x720')
+        self.title("MPS Chat")
         self.resizable(width=FALSE, height=FALSE)
         self.columnconfigure(1, weight=6)
         self.rowconfigure(0,weight=4)
@@ -43,7 +45,14 @@ class ChatGUI(Tk):
         # apri la prima chat nell'elenco
         # carica i messaggi della chat aperta
 
-# chat = ChatGUI()
-#
-# chat.chatList.addChatListElement("Rododendro", "Oggi piove", "18:12")
-# chat.mainloop()
+if __name__ == '__main__':
+    if os.getcwd().find("Client") == -1:
+        os.chdir("Client")
+
+    chat = ChatGUI()
+    client = Client("", 6555)
+    chat.createWidgets(client)
+
+    chat.chatList.addChatListElement("Rododendro", "Oggi piove", "18:12")
+    chat.iconbitmap(os.getcwd() + '/Images/windowIcon.ico')
+    chat.mainloop()
--- Server/test_server.py
+++ Server/test_server.py
@@ -1,11 +1,7 @@
 from Server import Server
 from Database.Database import Database
 s = Server(6000)
-db = Database('localhost',3306,'root','rootroot','messaggistica_mps')
-"""
 try:
     s.start()
 except KeyboardInterrupt:
     s.close()
-"""
-db.getMessageByReceiver("a")
