b89ed7912583c589ea40dde6e6231fd49b7c7b5d
maghero <37127779+maghero@users.noreply.github.com>
2018-12-03 21:33:08 +0100

    commenti -> ultimo cambio pc si spera

--- Client/Security/SecurityClient.py
+++ Client/Security/SecurityClient.py
@@ -51,22 +51,31 @@ class SecurityClient:
                     self.privateKey = serialization.load_pem_private_key(pem.read(),password=b'keyClients',backend=default_backend())
                     self.publicKey = self.privateKey.public_key()
                     self.clientKeys[username] = self.publicKey
-                    print('succesfully obtained key, path ' + path)
                 except ValueError:
                     print('utente non ha più la chiave privata')
         except FileNotFoundError:
             print('chiave persa')
 
     def generate_key(self):
+        """
+            Creates the private key for the client
+        """
         self.privateKey = rsa.generate_private_key(public_exponent=65537,\
                                                key_size=2048,\
                                                backend=default_backend())
         self.publicKey = self.privateKey.public_key()
-        #print(len(self.privateKey))
-        #print(len(self.publicKey.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)))
 
     def RSAEncryptText(self,text, key):
-        #print('len mes:' + str(len(text)))
+        """
+            Encrypt the text passed with the passed key
+
+            :type text: Bytes
+            :param text: the text that must be encrypted
+            :type key: Bytes
+            :param key: the key that must be used for encrypt with RSA
+            :rtype: Bytes
+            :return: the encrypted text
+        """
         cipherText = key.encrypt(text,
                                 padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                              algorithm=hashes.SHA256(),
@@ -75,12 +84,29 @@ class SecurityClient:
                                  )
         return cipherText
 
-    def getDigest(self,textBit):
+    def getDigest(self,text):
+        """
+            Apply the hash to the text passed
+
+            :type text: Bytes
+            :param text: the text
+            :rtype: Bytes
+            :return: The digest
+        """
         h = hashes.Hash(hashes.SHA256(), backend=default_backend())
-        h.update(textBit)
+        h.update(text)
         return h.finalize()
 
     def RSADecryptText(self,cipherText):
+        """
+            Perform the decryption of the passed cipherText with the private key
+            of the client
+
+            :type cipherText: Bytes
+            :param cipherText: The cipher text
+            :rtype: Bytes
+            :return: The plain text
+        """
         plaintext = self.privateKey.decrypt(cipherText,
                                             padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                                          algorithm=hashes.SHA256(),
@@ -90,6 +116,14 @@ class SecurityClient:
         return plaintext
 
     def getSignature(self,text):
+        """
+            Sign the passed text with the private key of the client
+
+            :type text: Bytes
+            :param text: The text that must be signed
+            :rtype: Bytes
+            :return: the signature
+        """
         try:
             signature = self.privateKey.sign(text,
                                          padding.PSS(mgf=padding.MGF1(hashes.SHA256()),
@@ -102,6 +136,20 @@ class SecurityClient:
             return None
 
     def VerifySignature(self,text,signature, user=None):
+        """
+            Performs the validation of the passed signature on the passed text using
+            the public key of the server if the user is None, else uses the public key
+            of the passed user
+
+            :type text: Bytes
+            :param text: the text to check the signature
+            :type signature: Bytes
+            :param signature: the signature that must be verify
+            :type user: String or None
+            :param user: The username of the user that had signed the text, if it's None the signature is of the server
+            :rtype: Boolean
+            :return: True if the signature is valide, else False
+        """
         try:
             if user is None:
                 self.serverPublicKey.verify(signature,text,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
@@ -109,40 +157,74 @@ class SecurityClient:
                 self.clientKeys[user].verify(signature,text,padding.PSS(mgf=padding.MGF1(hashes.SHA256()),salt_length=padding.PSS.MAX_LENGTH),hashes.SHA256())
             return True
         except InvalidSignature as e:
-            print('firma errata')
             return False
 
     def getServerPublicKey(self):
+        """
+            Used to get the public key of the server
+
+            :rtype: Bytes
+            :return: The public key of the server
+        """
         return self.serverPublicKey.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
     def getSerializedPublicKey(self):
+        """
+            Used to get the public key of the client
+
+            :rtype: Bytes
+            :return: The public key of the client
+        """
         return self.publicKey.public_bytes(encoding=serialization.Encoding.PEM,format=serialization.PublicFormat.SubjectPublicKeyInfo)
 
-    def AddSymmetricKeyFromDict(self,dict):
-        """Must be passed as argument the dict located at message['key']"""
-        self.SymmetricKey = dict.to_bytes(16, byteorder='big')
+    def AddServerSymmetricKey(self,key):
+        """
+            Add the symmetric key for the conversation with the server
+            :type key: Int
+            :param key: the symmetric key
+        """
+        self.SymmetricKey = key.to_bytes(16, byteorder='big')
 
     def getSymmetricKey(self):
+        """
+            obtain the symmetricKey of the conversation with the server
+
+            :rtype: Bytes
+            :return: The symmetric key
+        """
         return self.SymmetricKey;
 
-    def AESDecryptText(self,ct,username = None, nonce = None):
+    def AESDecryptText(self,cipherText,username = None, nonce = None):
+        """
+            Decrypt the passed cipherText with AES, if the nonce is not None, then
+            uses the symmetricKey to send a message to the server, if there is a specified
+            username then it uses the nonce of the client because the message is been received
+            from the server for the Online Key Exchange Protocol, else it uses the nonce with the
+            server. If there is a specified nonce then it uses the symmetricKey of the client passed
+            as username and the nonce passed
+
+            :type cipherText: Bytes
+            :param cipherText: the cipher text that must be decrypted
+            :type username: String or None
+            :param username: None if we have to use the symmetric key of the server
+
+        """
         try:
             if nonce is not None:
+                #decrypt with the symmetric key with the server
                 aescgm = AESGCM(self.SymmetricKey)
                 if username is None:
                     self.serverNonce = self.serverNonce+1
-                    #print('decryptnonceserver ' + str(self.serverNonce))
-                    return aescgm.decrypt(self.serverNonce.to_bytes(16, byteorder="big"),ct,None)
+                    return aescgm.decrypt(self.serverNonce.to_bytes(16, byteorder="big"),cipherText,None)
                 else:
                     self.clientNonce[username] = self.clientNonce[username]+1
-                    #print('decryptnonceserver ' + str(self.clientNonce[username]))
-                    return aescgm.decrypt(self.clientNonce[username].to_bytes(16, byteorder="big"),ct,None)
+                    return aescgm.decrypt(self.clientNonce[username].to_bytes(16, byteorder="big"),cipherText,None)
             else:
+                #decrypt with the symmetric key with the client - username
                 aescgm = AESGCM(self.clientSymmetricKeys[username])
                 self.clientNonce[username] = self.clientNonce[username]+1
                 print('decripto con AES , nonce '+ str(self.clientNonce[username]))
-                #print('decryptnonceclient ' + str(self.clientNonce[username]))
-                return aescgm.decrypt(self.clientNonce[username].to_bytes(16, byteorder="big"),ct,None)
+                return aescgm.decrypt(self.clientNonce[username].to_bytes(16, byteorder="big"),cipherText,None)
         except TypeError as t:
             print(t)
             print("Error in decrypt GCM")
